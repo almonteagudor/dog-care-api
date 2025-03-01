d3622f8c6ff782d82e81a361ab91735ea0840611
almonteagudor <55769333+almonteagudor@users.noreply.github.com>
2025-03-01 02:16:17 +0100

    fix nginx entrypoint.sh

--- docker/nginx/entrypoint.sh
+++ docker/nginx/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-envsubst '$PORT $HOST $PHP_HOST $PHP_PORT ' < /etc/nginx/templates/default.conf.template > /etc/nginx/conf.d/default.conf
+envsubst '$PORT $HOST $PHP_HOST $PHP_PORT ' < /etc/nginx/templates/default.template.conf > /etc/nginx/conf.d/default.conf
 
 exec "$@"
